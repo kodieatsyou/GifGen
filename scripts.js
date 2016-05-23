e8b2975a5a7fa79430fe06409d4a5e8494d65f7e
Tanner May <tannerjmay@gmail.com>
2016-05-23 07:53:37 -0600

    Removed title and rating search, got tag search to work

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
   var tags = null;
-  var rating = null;
-  var title = null;
 $("#genrand").click(function(){
 	tags = $("#tags").val();
-	rating= $("#rating").val();
-  title = $("#title").val();
   console.log(tags);
-  console.log(rating);
-  console.log(title);
+  q = tags; // search query
+	
+	request = new XMLHttpRequest;
+	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
+	
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400){
+			data = JSON.parse(request.responseText).data.image_url;
+			console.log(data);
+			document.getElementById("giphyme").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
+		} else {
+			console.log('reached giphy, but API returned an error');
+		 }
+	};
+
+	request.onerror = function() {
+		console.log('connection error');
+	};
+
+	request.send();
 });
 });
